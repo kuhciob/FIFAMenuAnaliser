43f7289da07609ed5c186c24a0c38d49348670e8
lvasuk <bivanv11@gmail.com>
2020-08-18 11:46:13 +0300

    Image verification

---
+++ FIFAImage.cs
@@ -0,0 +1,68 @@
+﻿
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIFAImageAnaliser
+{
+    class FIFAImage
+    {
+        private string imagePath;
+        private bool isValide;
+        OpenCvSharp.Point minLoc;
+        private IAnalysisrStratege algorithm;
+
+
+        public FIFAImage(string imgpath)
+        {
+            imagePath = imgpath;
+        }
+        public bool Verify()
+        {
+            string patern1Path = "images\\patterns\\CUSTOMIZE.png";
+            string patern2Path = "images\\patterns\\NewItem.png";
+
+
+            string mutchedPuth = Verify(patern1Path);
+            mutchedPuth+= Verify(patern1Path);
+
+            switch (mutchedPuth)
+            {
+                case 
+            }
+            return isValide;
+        }
+        private string Verify(string patternpath)
+        {
+            var image = new Mat(imagePath);
+            var template = new Mat(patternpath);
+
+            var w = (image.Width - template.Width) + 1;
+            var h = (image.Height - template.Height) + 1;
+
+            double minVal, maxVal;
+            
+            OpenCvSharp.Point minLoc, maxLoc;
+
+            var result = image.MatchTemplate(template, TemplateMatchModes.CCoeffNormed);
+
+            result.MinMaxLoc(out minVal, out maxVal, out minLoc, out maxLoc);
+            string VaryName = Path.GetFileNameWithoutExtension(patternpath);
+            if (maxVal == 1)
+            {
+                MessageBox.Show($"It is {VaryName} maxLoc: {maxLoc}, maxVal: {maxVal}");
+                return patternpath;    
+            }
+          
+            return "";
+        }
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIFAImageAnaliser
+{
+    public partial class Form1 : Form
+    {
+        private List<FIFAImage> Images=new List<FIFAImage>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        Image img = Image.FromFile(filePath);
+                        pictureBox1.Image = img;
+                        Images = new List<FIFAImage>();
+                        Images.Add(new FIFAImage(filePath));
+                    }
+                    catch (OutOfMemoryException ex)
+                    {
+                        MessageBox.Show("File is not a image");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File dosen`t exist");
+                }
+
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var img in Images)
+                img.Verify();
+        }
+    }
+}
